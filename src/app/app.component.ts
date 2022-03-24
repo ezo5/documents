00f093a89917b1b8a7f3f69e3fb61ba0bfb2eaaa
Gajdošík <jokirek@gmail.com>
2022-03-24 13:06:22 +0100

    initial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,6 +10,7 @@ import {DatePipe, DecimalPipe} from '@angular/common';
 export class AppComponent {
     public data: object = {};
     public template = '';
+    public color = '#00A4ED';
 
     constructor(private cdr: ChangeDetectorRef, private httpClient: HttpClient, private datePipe: DatePipe, private decimalPipe: DecimalPipe) {
         this.httpClient.get('assets/data.json').subscribe(data => {
@@ -19,16 +20,19 @@ export class AppComponent {
                     return datePipe.transform(render(text), 'dd.MM.YYYY') as string;
                 },
                 formatNumber: () => (text: string, render: (text: any) => any) => {
-                    return decimalPipe.transform(render(parseInt(text, 10))) || render(text) as string;
+                    return decimalPipe.transform(render(parseInt(text, 10))) || (render(text) as string);
                 },
                 queryString: () => (text: string, render: (text: any) => any) => {
                     return 'http://localhost:82/' + render(text);
+                },
+                companyColor: () => (text: string, render: (text: any) => any) => {
+                    return this.color;
                 }
             };
             this.cdr.markForCheck();
         });
 
-        this.httpClient.get('assets/header.html', {responseType: 'text'}).subscribe(html => {
+        this.httpClient.get('assets/header-sm.html', {responseType: 'text'}).subscribe(html => {
             this.template = html;
             this.cdr.markForCheck();
         });
